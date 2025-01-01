36ae7fe3a796c2fcc0be9152450ceb5f5699f696
Bryan Choate <bryan.choate@gmail.com>
2025-01-01 15:26:45 -0500

    checkpoint

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -1,15 +1,7 @@
-import os
-
 from app.api.routes import auth_routes, user_routes
 from fastapi import FastAPI
 
 app = FastAPI(title="User Service", version="1.0.0")
-
-# Access the secret key
-SECRET_KEY = os.getenv("SECRET_KEY")
-
-if not SECRET_KEY:
-    raise ValueError("SECRET_KEY is not set in the environment variables!")
 
 # Include routers
 app.include_router(auth_routes.router, prefix="/auth", tags=["auth"])
