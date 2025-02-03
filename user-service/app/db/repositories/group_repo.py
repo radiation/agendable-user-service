9e7a46cfe92885c3666d5e955ce3ea356359b8ac
Bryan Choate <bryan.choate@gmail.com>
2025-02-03 15:14:57 -0500

    Remove user-service

--- user-service/app/db/repositories/group_repo.py
+++
@@ -1,8 +0,0 @@
-from app.db.models import Group
-from app.db.repositories.base_repo import BaseRepository
-from sqlalchemy.ext.asyncio import AsyncSession
-
-
-class GroupRepository(BaseRepository[Group]):
-    def __init__(self, db: AsyncSession):
-        super().__init__(model=Group, db=db)
--- user-service/app/db/repositories/role_repo.py
+++
@@ -1,8 +0,0 @@
-from app.db.models import Role
-from app.db.repositories.base_repo import BaseRepository
-from sqlalchemy.ext.asyncio import AsyncSession
-
-
-class RoleRepository(BaseRepository[Role]):
-    def __init__(self, db: AsyncSession):
-        super().__init__(model=Role, db=db)
